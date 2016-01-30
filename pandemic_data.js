4befe31561c0f0bf753a77603affd3d50f38686c
mikemommsen <mikemommsen@gmail.com>
2016-01-30 08:30:56 -0600

    making a js file to try out jsfiddle

---
+++ pandemic_data.js
@@ -0,0 +1,486 @@
+data = [
+    {
+        "name": "algiers",
+        "color": "black",
+        "edges": [
+            "madrid",
+            "paris",
+            "istanbul",
+            "cairo"
+        ]
+    },
+    {
+        "name": "istanbul",
+        "color": "black",
+        "edges": [
+            "algiers",
+            "cairo",
+            "baghdad",
+            "moscow",
+            "st petersburg",
+            "milan"
+        ]
+    },
+    {
+        "name": "cairo",
+        "color": "black",
+        "edges": [
+            "algiers",
+            "istanbul",
+            "baghdad",
+            "riyadh",
+            "khartoum"
+        ]
+    },
+    {
+        "name": "riyadh",
+        "color": "black",
+        "edges": [
+            "cairo",
+            "baghdad",
+            "karachi"
+        ]
+    },
+    {
+        "name": "baghdad",
+        "color": "black",
+        "edges": [
+            "cairo",
+            "istanbul",
+            "moscow",
+            "tehran",
+            "riyadh"
+        ]
+    },
+    {
+        "name": "mumbai",
+        "color": "black",
+        "edges": [
+            "karachi",
+            "delhi",
+            "chennai"
+        ]
+    },
+    {
+        "name": "karachi",
+        "color": "black",
+        "edges": [
+            "baghdad",
+            "riyadh",
+            "mumbai",
+            "delhi",
+            "tehran"
+        ]
+    },
+    {
+        "name": "tehran",
+        "color": "black",
+        "edges": [
+            "moscow",
+            "baghdad",
+            "karachi",
+            "delhi"
+        ]
+    },
+    {
+        "name": "delhi",
+        "color": "black",
+        "edges": [
+            "tehran",
+            "karachi",
+            "chennai",
+            "kolkata",
+            "mumbai"
+        ]
+    },
+    {
+        "name": "kolkata",
+        "color": "black",
+        "edges": [
+            "delhi",
+            "chennai",
+            "bangkok",
+            "hong kong"
+        ]
+    },
+    {
+        "name": "chennai",
+        "color": "black",
+        "edges": [
+            "mumbai",
+            "delhi",
+            "kolkata",
+            "bangkok",
+            "jakarta"
+        ]
+    },
+    {
+        "name": "chicago",
+        "color": "blue",
+        "edges": [
+            "atlanta",
+            "mexico city",
+            "montreal",
+            "san francisco",
+            "los angeles"
+        ]
+    },
+    {
+        "name": "montreal",
+        "color": "blue",
+        "edges": [
+            "chicago",
+            "washington",
+            "new york"
+        ]
+    },
+    {
+        "name": "atlanta",
+        "color": "blue",
+        "edges": [
+            "miami",
+            "washington",
+            "chicago"
+        ]
+    },
+    {
+        "name": "washington",
+        "color": "blue",
+        "edges": [
+            "atlanta",
+            "miami",
+            "montreal",
+            "new york"
+        ]
+    },
+    {
+        "name": "new york",
+        "color": "blue",
+        "edges": [
+            "montreal",
+            "washington",
+            "madrid",
+            "london"
+        ]
+    },
+    {
+        "name": "london",
+        "color": "blue",
+        "edges": [
+            "new york",
+            "madrid",
+            "paris",
+            "essen"
+        ]
+    },
+    {
+        "name": "paris",
+        "color": "blue",
+        "edges": [
+            "london",
+            "essen",
+            "madrid",
+            "algiers",
+            "milan"
+        ]
+    },
+    {
+        "name": "essen",
+        "color": "blue",
+        "edges": [
+            "london",
+            "paris",
+            "milan",
+            "st petersburg"
+        ]
+    },
+    {
+        "name": "milan",
+        "color": "blue",
+        "edges": [
+            "paris",
+            "essen",
+            "istanbul"
+        ]
+    },
+    {
+        "name": "st petersburg",
+        "color": "blue",
+        "edges": [
+            "essen",
+            "istanbul",
+            "moscow"
+        ]
+    },
+    {
+        "name": "bangkok",
+        "color": "red",
+        "edges": [
+            "chennai",
+            "kolkata",
+            "hong kong",
+            "ho chi minh city",
+            "jakarta"
+        ]
+    },
+    {
+        "name": "jakarta",
+        "color": "red",
+        "edges": [
+            "chennai",
+            "bangkok",
+            "ho chi minh city",
+            "sydney"
+        ]
+    },
+    {
+        "name": "ho chi minh city",
+        "color": "red",
+        "edges": [
+            "jakarta",
+            "bangkok",
+            "hong kong",
+            "manila"
+        ]
+    },
+    {
+        "name": "manila",
+        "color": "red",
+        "edges": [
+            "san francisco",
+            "sydney",
+            "jakarta",
+            "taipei",
+            "hong kong"
+        ]
+    },
+    {
+        "name": "sydney",
+        "color": "red",
+        "edges": [
+            "los angeles",
+            "manila",
+            "jakarta"
+        ]
+    },
+    {
+        "name": "taipei",
+        "color": "red",
+        "edges": [
+            "manila",
+            "osaka",
+            "shanghai",
+            "hong kong"
+        ]
+    },
+    {
+        "name": "osaka",
+        "color": "red",
+        "edges": [
+            "tokyo",
+            "taipei"
+        ]
+    },
+    {
+        "name": "seoul",
+        "color": "red",
+        "edges": [
+            "beijing",
+            "shanghai",
+            "tokyo"
+        ]
+    },
+    {
+        "name": "san francisco",
+        "color": "yellow",
+        "edges": [
+            "los angeles",
+            "chicago",
+            "tokyo",
+            "manila"
+        ]
+    },
+    {
+        "name": "mexico city",
+        "color": "yellow",
+        "edges": [
+            "los angeles",
+            "lima",
+            "bogota",
+            "miami",
+            "chicago"
+        ]
+    },
+    {
+        "name": "miami",
+        "color": "yellow",
+        "edges": [
+            "mexico city",
+            "bogota",
+            "atlanta",
+            "washington"
+        ]
+    },
+    {
+        "name": "bogota",
+        "color": "yellow",
+        "edges": [
+            "lima",
+            "mexico city",
+            "miami",
+            "buenos aires",
+            "sao paulo"
+        ]
+    },
+    {
+        "name": "lima",
+        "color": "yellow",
+        "edges": [
+            "santiago",
+            "mexico city",
+            "bogota"
+        ]
+    },
+    {
+        "name": "santiago",
+        "color": "yellow",
+        "edges": [
+            "lima"
+        ]
+    },
+    {
+        "name": "buenos aires",
+        "color": "yellow",
+        "edges": [
+            "sao paulo",
+            "bogota"
+        ]
+    },
+    {
+        "name": "lagos",
+        "color": "yellow",
+        "edges": [
+            "sao paulo",
+            "kinshasa",
+            "khartoum"
+        ]
+    },
+    {
+        "name": "johannesburg",
+        "color": "yellow",
+        "edges": [
+            "khartoum",
+            "kinshasa"
+        ]
+    },
+    {
+        "name": "sao paulo",
+        "color": "yellow",
+        "edges": [
+            "lagos",
+            "buenos aires",
+            "madrid",
+            "bogota"
+        ]
+    },
+    {
+        "name": "khartoum",
+        "color": "yellow",
+        "edges": [
+            "cairo",
+            "lagos",
+            "kinshasa",
+            "johannesburg"
+        ]
+    },
+    {
+        "name": "kinshasa",
+        "color": "yellow",
+        "edges": [
+            "lagos",
+            "johannesburg",
+            "khartoum"
+        ]
+    },
+    {
+        "color": "black",
+        "name": "moscow",
+        "edges": [
+            "st petersburg",
+            "istanbul",
+            "tehran"
+        ]
+    },
+    {
+        "color": "red",
+        "name": "tokyo",
+        "edges": [
+            "seoul",
+            "shanghai",
+            "osaka",
+            "san francisco"
+        ]
+    },
+    {
+        "color": "red",
+        "name": "shanghai",
+        "edges": [
+            "beijing",
+            "seoul",
+            "tokyo",
+            "hong kong",
+            "taipei"
+        ]
+    },
+    {
+        "color": "red",
+        "name": "beijing",
+        "edges": [
+            "shanghai",
+            "seoul"
+        ]
+    },
+    {
+        "color": "red",
+        "name": "hong kong",
+        "edges": [
+            "kolkata",
+            "bangkok",
+            "ho chi minh city",
+            "manila",
+            "taipei",
+            "shanghai"
+        ]
+    },
+    {
+        "color": "blue",
+        "name": "san francisco",
+        "edges": [
+            "chicago",
+            "los angeles",
+            "tokyo",
+            "manila"
+        ]
+    },
+    {
+        "color": "blue",
+        "name": "madrid",
+        "edges": [
+            "sao paulo",
+            "algiers",
+            "paris",
+            "new york",
+            "london"
+        ]
+    },
+    {
+      "name": "los angeles",
+      "color": "yellow",
+      "edges": [
+        "mexico city",
+        "san francisco",
+        "sydney",
+        "chicago"
+      ]
+    }
+]
